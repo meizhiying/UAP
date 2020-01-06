fda929b572d0cb39411a4a18963ed19faee84349
meizhiying <meizhiying@genomics.cn>
2020-01-06 18:37:59 +0800

    FirstCommit

--- bin/pyinstaller.sh
+++
@@ -1,2 +0,0 @@
-pyinstaller -F --hidden-import array --hidden-import pysam.libctabixproxies --hidden-import json --hidden-import ctypes UAP.py 
-#pyinstaller -F --hidden-import array --hidden-import pysam.libctabixproxies --hidden-import json --hidden-import ctypes UAP_test.py 
